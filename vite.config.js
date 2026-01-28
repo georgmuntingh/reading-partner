ab3814c2df806c7f6a34b8ced81a75c9fc5068e2
Georg Muntingh <georg.muntingh@gmail.com>
2026-01-28 11:54:32 +0100

    Change to Github Pages deploy from branch.

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,10 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   // Set the base path to match your GitHub repository name
-  base: '/reading-partner/',
+  // base: '/reading-partner/',
+
+  // If VITE_BASE_PATH is set (PR), use it. 
+  // Otherwise, use the standard production path.
+  base: process.env.VITE_BASE_PATH || '/reading-partner/',  
 })
+
